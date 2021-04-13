9859cba17fb4fbe7649ed92f03b8f61d08caa282
Amiya Ranjan <66031190+Sunny955@users.noreply.github.com>
2021-04-14 01:10:48 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css'
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+    
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="14a4d901-fcef-4eef-aa6c-190b6be85895"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
+}
+
+export default App;
